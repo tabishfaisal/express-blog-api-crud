0903de70e9fbcee027a077822fd38425a01a04e3
“tabihsfaisal“ <tabish.faisaltn@gmail.com>
2024-12-29 20:51:22 +0100

    added again

--- data/post.js
+++ data/post.js
@@ -1,4 +1,4 @@
-const posts = [
+let posts = [
     {
         id: 1,
         title: "Ricetta Ciambellone",
@@ -37,4 +37,4 @@ const posts = [
 ];
 
 
-module.exports = posts;
+export default  posts;
